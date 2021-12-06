835a3f2a7c43e45100e36623cc458f9ec5ca33a7
HamishWahlers <92501976+HamishWahlers@users.noreply.github.com>
2021-12-06 20:41:05 +1300

    Add files via upload

---
+++ 0-Lesson-Assessments-main/Assignment-02/js/index.js
@@ -0,0 +1,14 @@
+'use strict'
+//form validation logic goes here
+//pesudeo code:
+//Get reference to the html elements
+//add an event listener to the submit button
+//when the user presses the button, validate the inputs
+//if not valid, show errors
+//if valid, console.log('success)
+
+//comments:
+// This exercise relies heavily upon the lesson 11 file and video. Everything you need should be there.
+// That said, you have freedom in how you write your code and in how you approach the script
+// Intergrate this form into a new contact page of your assessment one website.
+//the css can be as minimal or complex as you like.
